03650f8c0cd8f1f06280aaf8ac81ccaa5bd34c13
Jared <a353391@uach.mx>
2023-04-04 15:53:21 -0600

    Agregando ejercicio 18

---
+++ 18.js
@@ -0,0 +1,13 @@
+const readlineSync = require('readline-sync');
+
+let phrase = readlineSync.question("Ingrese la frase: ");
+let letter = readlineSync.question("Ingrese la letra que desea buscar: ");
+let countLetter = 0;
+
+for (let i = 0; i < phrase.length; i++) {
+    if (phrase[i] === letter) {
+        countLetter++;
+    }
+}
+
+console.log(`El numero de veces que la letra \"${letter}\" aparece en la frase es: ${countLetter}`);
